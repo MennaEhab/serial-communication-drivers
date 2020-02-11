d92975fbd4139f0644a304a687c186cfcf849fd5
MennaEhab <mennaehab21.5@gmail.com>
2020-02-11 13:46:33 +0200

    update1

--- SPI__.h
+++ SPI__.h
@@ -14,12 +14,39 @@
 #include "registers.h"
 #include "gpio.h"
 
+#define ENABLE 1
+#define DISABLE 0
+
+#define MASTER ENABLE
+#define SLAVE  DISABLE
+
+#define SCK_PORT  GPIOB
+#define MISO_PORT GPIOB
+#define MOSI_PORT GPIOB
+#define SS_PORT   GPIOB
+
+#define SCK_PIN  BIT7
+#define MISO_PIN BIT6
+#define MOSI_PIN BIT5
+#define SS_PIN   BIT4
+
+
 typedef enum SPI_clk{
 	Fosc4 = 0x00 ,
 	Fosc16 = 0x01 ,
 	Fosc64 = 0x02 ,
-	Fosc128 = 0x03 ,
+	Fosc128 = 0x03 
 	}SPI_clk;
+
+typedef enum SPI_mode{
+	mode1 = 0x00 ,
+	mode2 = 0x04 ,
+	mode3 = 0x08 ,
+	mode4 = 0x0C 
+}SPI_mode;
+
+
+
 
 #define  Enable_SPI 0x40 
 
